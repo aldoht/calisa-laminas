c49d41340e2cd214f22613f6d36d45f59556e498
aldoht <aldofernandoht@gmail.com>
2025-01-10 12:34:10 -0600

    feat: auth with supabase jwt

--- Program.cs
+++ Program.cs
@@ -1,18 +1,40 @@
+using System.Text;
+
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options => 
+{
+    options.Conventions.AddPageRoute("/IniciarSesion/Index", "/");
+});
 
 var url = builder.Configuration["SUPABASE_URL"]!;
 var key = builder.Configuration["SUPABASE_KEY"];
 var options = new Supabase.SupabaseOptions
 {
-    AutoConnectRealtime = true
+    AutoConnectRealtime = true,
+    AutoRefreshToken = true
 };
 var supabase = new Supabase.Client(url, key, options);
 await supabase.InitializeAsync();
 
 builder.Services.AddSingleton(supabase);
+
+builder.Services.AddAuthorization();
+
+var bytes = Encoding.UTF8.GetBytes(builder.Configuration["JWT_SECRET"]!);
+builder.Services.AddAuthentication().AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(bytes),
+        ValidAudience = builder.Configuration["SUPABASE_VALID_AUDIENCE"],
+        ValidIssuer = builder.Configuration["SUPABASE_VALID_ISSUER"]
+    };
+});
 
 var app = builder.Build();
 
